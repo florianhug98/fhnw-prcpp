82dcbefd66d59b142ea60c593da6a862fe31c525
Florian Hug <florian.hug@unibe.ch>
2025-10-21 12:52:15 +0200

    formatting, variable naming and comments

--- Uebung1/bitmap/Bitmap/Bitmap.cpp
+++ Uebung1/bitmap/Bitmap/Bitmap.cpp
@@ -16,45 +16,47 @@ struct RGB;
 
 ///////////////////////////////////////////////////////////////////////////////
 struct RGB {
-public:
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
 
-    RGB(uint8_t r, uint8_t g, uint8_t b) : r(r), g(g), b(b) {}
+  RGB(const uint8_t r, const uint8_t g, const uint8_t b) : r(r), g(g), b(b) {
+  }
 
-    explicit RGB(const ARGB& argb);
+  explicit RGB(const ARGB &argb);
 };
 
-static_assert(sizeof(RGB) == 24/8);
+static_assert(sizeof(RGB) == 24 / 8);
 
 ///////////////////////////////////////////////////////////////////////////////
 
 struct ARGB {
-public:
-    uint8_t a;
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
+  uint8_t a;
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
 
-    ARGB(uint8_t r, uint8_t g, uint8_t b) : a(255), r(r), g(g), b(b) {}
-	explicit ARGB(const RGB& rgb);
+  ARGB(const uint8_t r, const uint8_t g, const uint8_t b) : a(255), r(r), g(g), b(b) {
+  }
+  explicit ARGB(const RGB &rgb);
 };
 
-static_assert(sizeof(ARGB) == 32/8);
+static_assert(sizeof(ARGB) == 32 / 8);
 
 ///////////////////////////////////////////////////////////////////////////////
-RGB::RGB(const ARGB& argb) : r(argb.r), g(argb.g), b(argb.b) {}
+RGB::RGB(const ARGB &argb) : r(argb.r), g(argb.g), b(argb.b) {
+}
 
 ///////////////////////////////////////////////////////////////////////////////
-ARGB::ARGB(const RGB& rgb) : a(255), r(rgb.r), g(rgb.g), b(rgb.b) {}
+ARGB::ARGB(const RGB &rgb) : a(255), r(rgb.r), g(rgb.g), b(rgb.b) {
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 /// <summary>
 /// Return image height in pixels
 /// </summary>
 uint32_t Bitmap::height() const {
-	return static_cast<uint32_t>(std::abs(m_infoHeader.m_height));
+  return static_cast<uint32_t>(std::abs(m_infoHeader.m_height));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -63,14 +65,13 @@ uint32_t Bitmap::height() const {
 /// </summary>
 /// <returns>number of bytes per image row</returns>
 uint32_t Bitmap::rowSize() const {
+  const auto width = static_cast<uint32_t>(std::abs(m_infoHeader.m_width));
 
-	uint32_t width = static_cast<uint32_t>(std::abs(m_infoHeader.m_width));
-
-	// width in pixel * bytes per pixel
-	uint32_t rawBytes = width * (m_infoHeader.m_bpp / 8);
-	// pads so the size is a multiple of 4
-	uint32_t pad = (4 - (rawBytes % 4)) % 4;
-	return rawBytes + pad;
+  // width in pixel * bytes per pixel
+  const uint32_t rawBytes = width * (m_infoHeader.m_bpp / 8);
+  // pads so the size is a multiple of 4
+  const uint32_t pad = (4 - (rawBytes % 4)) % 4;
+  return rawBytes + pad;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -79,7 +80,7 @@ uint32_t Bitmap::rowSize() const {
 /// </summary>
 /// <returns>image size in bytes</returns>
 uint32_t Bitmap::imageSize() const {
-	return height() * rowSize();
+  return height() * rowSize();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -89,46 +90,48 @@ uint32_t Bitmap::imageSize() const {
 /// <param name="filename">file path</param>
 /// <returns>true: reading was successful</returns>
 bool Bitmap::read(std::string_view filename) {
-	std::ifstream fs(filename.data(), std::ios::in | std::ios::binary);
+  std::ifstream fs(filename.data(), std::ios::in | std::ios::binary);
 
-	if (!fs) {
-		std::cerr << "File can't be opened for reading" << std::endl;
-		return false;
-	}
+  if (!fs) {
+    std::cerr << "File can't be opened for reading" << std::endl;
+    return false;
+  }
 
-	// read bitmap file header
-	if (!fs.read(reinterpret_cast<char*>(&m_fileHeader), sizeof(BitmapFileHeader)) || m_fileHeader.m_signature != BitmapSignature) {
-		std::cerr << "Invalid bitmap file header" << std::endl;
-		return false;
-	}
+  // read bitmap file header
+  if (!fs.read(reinterpret_cast<char *>(&m_fileHeader), sizeof(BitmapFileHeader)) || m_fileHeader.
+    m_signature != BitmapSignature) {
+    std::cerr << "Invalid bitmap file header" << std::endl;
+    return false;
+  }
 
-	// read bitmap info header
-	if (!fs.read(reinterpret_cast<char*>(&m_infoHeader), sizeof(BitmapInfoHeader)) || m_infoHeader.m_headerSize < sizeof(BitmapInfoHeader)) {
-		std::cerr << "Invalid bitmap info header" << std::endl;
-		return false;
-	}
+  // read bitmap info header
+  if (!fs.read(reinterpret_cast<char *>(&m_infoHeader), sizeof(BitmapInfoHeader)) || m_infoHeader.
+    m_headerSize < sizeof(BitmapInfoHeader)) {
+    std::cerr << "Invalid bitmap info header" << std::endl;
+    return false;
+  }
 
-	// check info header
-	if (m_infoHeader.m_bpp != 24 && m_infoHeader.m_bpp != 32) {
-		std::cerr << "Unsupported bitmap format" << std::endl;
-		return false;
-	}
-	if (m_infoHeader.m_imageSize != 0 && m_infoHeader.m_imageSize != imageSize()) {
-		std::cerr << "Invalid image size" << std::endl;
-		return false;
-	}
+  // check info header
+  if (m_infoHeader.m_bpp != 24 && m_infoHeader.m_bpp != 32) {
+    std::cerr << "Unsupported bitmap format" << std::endl;
+    return false;
+  }
+  if (m_infoHeader.m_imageSize != 0 && m_infoHeader.m_imageSize != imageSize()) {
+    std::cerr << "Invalid image size" << std::endl;
+    return false;
+  }
 
-	// jump to image data
-	fs.seekg(m_fileHeader.m_imageStart);
+  // jump to image data
+  fs.seekg(m_fileHeader.m_imageStart);
 
-	m_image = std::make_unique_for_overwrite<uint8_t[]>(imageSize());
+  m_image = std::make_unique_for_overwrite<uint8_t[]>(imageSize());
 
-	if (!fs.read(reinterpret_cast<char*>(m_image.get()), imageSize())) {
-		std::cerr << "Error reading image data" << std::endl;
-		return false;
-	}
+  if (!fs.read(reinterpret_cast<char *>(m_image.get()), imageSize())) {
+    std::cerr << "Error reading image data" << std::endl;
+    return false;
+  }
 
-	return true;
+  return true;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -138,44 +141,46 @@ bool Bitmap::read(std::string_view filename) {
 /// <param name="filename">file path</param>
 /// <returns>true: writing was successful</returns>
 bool Bitmap::write(std::string_view filename) const {
-	std::ofstream fs(filename.data(), std::ios::out | std::ios::binary);
+  std::ofstream fs(filename.data(), std::ios::out | std::ios::binary);
 
-	if (!fs) {
-		std::cerr << "File can't be opened for writing" << std::endl;
-		return false;
-	}
+  if (!fs) {
+    std::cerr << "File can't be opened for writing" << std::endl;
+    return false;
+  }
 
-	// check info header
-	if (m_infoHeader.m_bpp != 24 && m_infoHeader.m_bpp != 32) {
-		std::cerr << "Unsupported bitmap format" << std::endl;
-		return false;
-	}
-	if (m_infoHeader.m_imageSize != 0 && m_infoHeader.m_imageSize != imageSize()) {
-		std::cerr << "Invalid image size" << std::endl;
-		return false;
-	}
+  // check info header
+  if (m_infoHeader.m_bpp != 24 && m_infoHeader.m_bpp != 32) {
+    std::cerr << "Unsupported bitmap format" << std::endl;
+    return false;
+  }
+  if (m_infoHeader.m_imageSize != 0 && m_infoHeader.m_imageSize != imageSize()) {
+    std::cerr << "Invalid image size" << std::endl;
+    return false;
+  }
 
-	// write bitmap file header
-	if (!fs.write(reinterpret_cast<const char*>(&m_fileHeader), sizeof(BitmapFileHeader)) || m_fileHeader.m_signature != BitmapSignature) {
-		std::cerr << "Invalid bitmap file header" << std::endl;
-		return false;
-	}
+  // write bitmap file header
+  if (!fs.write(reinterpret_cast<const char *>(&m_fileHeader), sizeof(BitmapFileHeader)) ||
+    m_fileHeader.m_signature != BitmapSignature) {
+    std::cerr << "Invalid bitmap file header" << std::endl;
+    return false;
+  }
 
-	// write bitmap info header
-	if (!fs.write(reinterpret_cast<const char*>(&m_infoHeader), sizeof(BitmapInfoHeader)) || m_infoHeader.m_headerSize != sizeof(BitmapInfoHeader)) {
-		std::cerr << "Invalid bitmap info header" << std::endl;
-		return false;
-	}
+  // write bitmap info header
+  if (!fs.write(reinterpret_cast<const char *>(&m_infoHeader), sizeof(BitmapInfoHeader)) ||
+    m_infoHeader.m_headerSize != sizeof(BitmapInfoHeader)) {
+    std::cerr << "Invalid bitmap info header" << std::endl;
+    return false;
+  }
 
-	// jump to image data
-	fs.seekp(m_fileHeader.m_imageStart);
+  // jump to image data
+  fs.seekp(m_fileHeader.m_imageStart);
 
-	if (!fs.write(reinterpret_cast<const char*>(m_image.get()), imageSize())) {
-		std::cerr << "Error writing image data" << std::endl;
-		return false;
-	}
+  if (!fs.write(reinterpret_cast<const char *>(m_image.get()), imageSize())) {
+    std::cerr << "Error writing image data" << std::endl;
+    return false;
+  }
 
-	return true;
+  return true;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -183,11 +188,11 @@ bool Bitmap::write(std::string_view filename) const {
 /// Invert this image
 /// </summary>
 void Bitmap::invert() const {
-	if (m_image) {
-		for (uint32_t i = 0; i < imageSize(); ++i) {
-			m_image[i] = ~m_image[i];
-		}
-	}
+  if (m_image) {
+    for (uint32_t i = 0; i < imageSize(); ++i) {
+      m_image[i] = ~m_image[i];
+    }
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -195,15 +200,17 @@ void Bitmap::invert() const {
 /// Flip this image vertically
 /// </summary>
 void Bitmap::verFlip() const {
-	if (m_image) {
-		for (uint32_t i = 0; i < height() / 2; ++i) {
-			uint8_t* pRow1 = m_image.get() + i * rowSize();
-			uint8_t* pRow2 = m_image.get() + (height() - 1 - i) * rowSize();
-			for (uint32_t j = 0; j < rowSize(); ++j) {
-				std::swap(pRow1[j], pRow2[j]);
-			}
-		}
-	}
+  if (m_image) {
+    for (uint32_t i = 0; i < height() / 2; ++i) {
+      // pointer to start of row
+      uint8_t *pRow1 = m_image.get() + i * rowSize();
+      // pointer to start of 'opposite' row of pRow1
+      uint8_t *pRow2 = m_image.get() + (height() - 1 - i) * rowSize();
+      for (uint32_t j = 0; j < rowSize(); ++j) {
+        std::swap(pRow1[j], pRow2[j]);
+      }
+    }
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -211,21 +218,24 @@ void Bitmap::verFlip() const {
 /// Flip this image horizontally
 /// </summary>
 void Bitmap::horFlip() const {
-	if (m_image) {
-		uint32_t w = static_cast<uint32_t>(std::abs(m_infoHeader.m_width));
-		uint32_t bytesPerPixel = m_infoHeader.m_bpp / 8;
-		for (uint32_t y = 0; y < height(); ++y) {
-			uint8_t* pRow = m_image.get() + y * rowSize();
-			uint32_t wLast = w - 1;
-			for (uint32_t i = 0; i < w / 2; ++i) {
-				uint8_t* pPixel1 = pRow + i * bytesPerPixel;
-				uint8_t* pPixel2 = pRow + (wLast - i) * bytesPerPixel;
-				for (uint32_t b = 0; b < bytesPerPixel; ++b) {
-					std::swap(pPixel1[b], pPixel2[b]);
-				}
-			}
-		}
-	}
+  if (m_image) {
+    const auto w = static_cast<uint32_t>(std::abs(m_infoHeader.m_width));
+    const uint32_t bytesPerPixel = m_infoHeader.m_bpp / 8;
+    for (uint32_t y = 0; y < height(); ++y) {
+      // pointer to start of row
+      uint8_t *pRow = m_image.get() + y * rowSize();
+      const uint32_t wLast = w - 1;
+      for (uint32_t i = 0; i < w / 2; ++i) {
+        // pointer to pixel in row
+        uint8_t *pPixel1 = pRow + i * bytesPerPixel;
+        // pointer to 'oposite' pixel in row
+        uint8_t *pPixel2 = pRow + (wLast - i) * bytesPerPixel;
+        for (uint32_t b = 0; b < bytesPerPixel; ++b) {
+          std::swap(pPixel1[b], pPixel2[b]);
+        }
+      }
+    }
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -234,47 +244,42 @@ void Bitmap::horFlip() const {
 /// </summary>
 /// <returns>converted image</returns>
 Bitmap Bitmap::cvtToRGB() const {
-	Bitmap out{};
+  Bitmap out{};
 
-	// if image is ARGB
-	if (m_image && m_infoHeader.m_bpp == 32) {
+  // if image is ARGB
+  if (m_image && m_infoHeader.m_bpp == 32) {
+    out.m_fileHeader = m_fileHeader;
+    out.m_infoHeader = m_infoHeader;
 
-		out.m_fileHeader = m_fileHeader;
-		out.m_infoHeader = m_infoHeader;
+    // Adjust for RGB format
+    out.m_infoHeader.m_bpp = 24;
+    out.m_infoHeader.m_imageSize = out.imageSize();
+    out.m_fileHeader.m_fileSize = out.m_fileHeader.m_imageStart + out.m_infoHeader.m_imageSize;
 
-		// Adjust for RGB format
-		out.m_infoHeader.m_bpp = 24;
-		out.m_infoHeader.m_imageSize = out.imageSize();
-		out.m_fileHeader.m_fileSize = out.m_fileHeader.m_imageStart + out.m_infoHeader.m_imageSize;
+    out.m_image = std::make_unique_for_overwrite<uint8_t[]>(out.imageSize());
 
-		out.m_image = std::make_unique_for_overwrite<uint8_t[]>(out.imageSize());
+    const auto w = static_cast<uint32_t>(std::abs(m_infoHeader.m_width));
 
-		uint32_t w = static_cast<uint32_t>(std::abs(m_infoHeader.m_width));
-		uint32_t h = height();
+    for (uint32_t y = 0; y < height(); ++y) {
+      const uint8_t *pInRow = m_image.get() + y * rowSize();
+      uint8_t *pOutRow = out.m_image.get() + y * out.rowSize();
 
-		std::cout << w << std::endl;
-		std::cout << rowSize() / 4 << std::endl;
+      for (uint32_t x = 0; x < w; ++x) {
+        const uint8_t *pInPixel = pInRow + x * 4;
+        ARGB argb(pInPixel[2], pInPixel[1], pInPixel[0]);
 
-		for (uint32_t y = 0; y < h; ++y) {
-			const uint8_t* inRow = m_image.get() + y * rowSize();
-			uint8_t* outRow = out.m_image.get() + y * out.rowSize();
+        // Convert to RGB
+        const RGB rgb(argb);
 
-			for (uint32_t x = 0; x < w; ++x) {
-				const uint8_t* inPixel = inRow + x * 4;
-				ARGB argb(inPixel[2], inPixel[1], inPixel[0]);
+        uint8_t *pOutPixel = pOutRow + x * 3;
+        pOutPixel[0] = rgb.b;
+        pOutPixel[1] = rgb.g;
+        pOutPixel[2] = rgb.r;
+      }
+    }
+  }
 
-				// Convert to RGB
-				RGB rgb(argb);
-
-				uint8_t* outPixel = outRow + x * 3;
-				outPixel[0] = rgb.b;
-				outPixel[1] = rgb.g;
-				outPixel[2] = rgb.r;
-			}
-		}
-	}
-
-	return out;
+  return out;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -283,47 +288,43 @@ Bitmap Bitmap::cvtToRGB() const {
 /// </summary>
 /// <returns>converted image</returns>
 Bitmap Bitmap::cvtToARGB() const {
-	Bitmap out{};
+  Bitmap out{};
 
-	// if image is RGB
-	if (m_image && m_infoHeader.m_bpp == 24) {
+  // if image is RGB
+  if (m_image && m_infoHeader.m_bpp == 24) {
+    out.m_fileHeader = m_fileHeader;
+    out.m_infoHeader = m_infoHeader;
 
-		out.m_fileHeader = m_fileHeader;
-		out.m_infoHeader = m_infoHeader;
+    // Adjust for ARGB format
+    out.m_infoHeader.m_bpp = 32;
+    out.m_infoHeader.m_imageSize = out.imageSize();
+    out.m_fileHeader.m_fileSize = out.m_fileHeader.m_imageStart + out.m_infoHeader.m_imageSize;
 
-		// Adjust for ARGB format
-		out.m_infoHeader.m_bpp = 32;
-		out.m_infoHeader.m_imageSize = out.imageSize();
-		out.m_fileHeader.m_fileSize = out.m_fileHeader.m_imageStart + out.m_infoHeader.m_imageSize;
+    out.m_image = std::make_unique_for_overwrite<uint8_t[]>(out.imageSize());
 
-		out.m_image = std::make_unique_for_overwrite<uint8_t[]>(out.imageSize());
+    const auto w = static_cast<uint32_t>(std::abs(m_infoHeader.m_width));
 
-		uint32_t w = static_cast<uint32_t>(std::abs(m_infoHeader.m_width));
+    // iterate through rows
+    for (uint32_t y = 0; y < height(); ++y) {
+      const uint8_t *pInRow = m_image.get() + y * rowSize();
+      uint8_t *pOutRow = out.m_image.get() + y * out.rowSize();
 
-		std::cout << w << std::endl;
-		std::cout << rowSize() / 3 << std::endl;
+      // iterate through pixels in given row
+      for (uint32_t x = 0; x < w; ++x) {
+        const uint8_t *pInPixel = pInRow + x * 3;
+        RGB rgb(pInPixel[2], pInPixel[1], pInPixel[0]);
 
-		// iterate through rows
-		for (uint32_t y = 0; y < height(); ++y) {
-			const uint8_t* inRow = m_image.get() + y * rowSize();
-			uint8_t* outRow = out.m_image.get() + y * out.rowSize();
+        // Convert to ARGB
+        const ARGB argb(rgb);
 
-			// iterate through pixels in given row
-			for (uint32_t x = 0; x < w; ++x) {
-				const uint8_t* inPixel = inRow + x * 3;
-				RGB rgb(inPixel[2], inPixel[1], inPixel[0]);
+        uint8_t *pOutPixel = pOutRow + x * 4;
+        pOutPixel[0] = argb.b;
+        pOutPixel[1] = argb.g;
+        pOutPixel[2] = argb.r;
+        pOutPixel[3] = argb.a;
+      }
+    }
+  }
 
-				// Convert to ARGB
-				ARGB argb(rgb);
-
-				uint8_t* outPixel = outRow + x * 4;
-				outPixel[0] = argb.b;
-				outPixel[1] = argb.g;
-				outPixel[2] = argb.r;
-				outPixel[3] = argb.a;
-			}
-		}
-	}
-
-	return out;
+  return out;
 }
